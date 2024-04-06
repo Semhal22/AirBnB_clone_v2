3ac66fb2da678aa3507f86ce1eff959c861ae1de
Semhal Gidena <semhalgidena@gmail.com>
2024-04-06 12:44:04 +0000

    Set up web server for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,31 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
+# Sets up web server for deployment of web_static
+apt-get -y update > /dev/null 2>&1;
+apt-get -y install nginx > /dev/null 2>&1;
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+
+cat <<EOF > /data/web_static/releases/test/index.html
+<html>
+    <head></head>
+    <body>
+      ALX
+    </body>
+</html>
+EOF
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+cat <<EOF > /etc/nginx/sites-enabled/default
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+        server_name _;
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+        location /hbnb_static {
+		alias /data/web_static/current/; 
+        }
+}
+EOF
+nginx -s reload
